42022fd81245ebedb5cfc1403774ad0f3fcab02a
Albert Puigsech <albert@puigsech.com>
2016-08-18 14:40:30 +0300

    add missing source.go file

---
+++ lib/source.go
@@ -0,0 +1,16 @@
+package lib
+
+type LoadOptions map[string]interface{}
+
+type SourceType interface {
+	LoadObjects(source string, opt LoadOptions) ([]Object, error)
+}
+
+func (s *Seekret) LoadObjects(st SourceType, source string, opt LoadOptions) error {
+	objectList, err := st.LoadObjects(source, opt)
+	if err != nil {
+		return err
+	}
+	s.objectList = append(s.objectList, objectList...)
+	return nil
+}
